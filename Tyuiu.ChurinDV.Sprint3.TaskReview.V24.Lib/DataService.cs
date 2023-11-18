a3c6dff043fd136d3131ad8769d52f320fac91a5
ChurinDmitriy <mister.dim2017111@gmail.com>
2023-11-18 14:54:18 +0500

    TaskReview

---
+++ Tyuiu.ChurinDV.Sprint3.TaskReview.V24.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChurinDV.Sprint3.TaskReview.V24.Lib
+{
+    public class DataService : ISprint3Task7V24
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] array;
+            int len = (stopValue - startValue) + 1;
+            array = new double[len];
+            int count = 0;
+            double y = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x + 1.2 == 0)
+                {
+                    array[count] = 0;
+                }
+                else
+                {
+                    y = Math.Round((Math.Sin(x) / (x + 1.2)) - Math.Sin(x) * 2 - 2 * x, 2);
+                    array[count] = y;
+                    count++;
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ Tyuiu.ChurinDV.Sprint3.TaskReview.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChurinDV.Sprint3.TaskReview.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChurinDV.Sprint3.TaskReview.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6772510-0537-44c1-b7ee-d967f8b70a39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
